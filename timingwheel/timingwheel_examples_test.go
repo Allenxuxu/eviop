6578d5301e29c0295da6c85387d5768255f14e7d
Allenxuxu <120582243@qq.com>
2019-08-26 01:25:15 +0800

    add timingwheel

---
+++ timingwheel/timingwheel_examples_test.go
@@ -0,0 +1,42 @@
+package timingwheel_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/RussellLuo/timingwheel"
+)
+
+func Example_startTimer() {
+	tw := timingwheel.NewTimingWheel(time.Millisecond, 20)
+	tw.Start()
+	defer tw.Stop()
+
+	exitC := make(chan time.Time, 1)
+	tw.AfterFunc(time.Second, func() {
+		fmt.Println("The timer fires")
+		exitC <- time.Now()
+	})
+
+	<-exitC
+
+	// Output:
+	// The timer fires
+}
+
+func Example_stopTimer() {
+	tw := timingwheel.NewTimingWheel(time.Millisecond, 20)
+	tw.Start()
+	defer tw.Stop()
+
+	t := tw.AfterFunc(time.Second, func() {
+		fmt.Println("The timer fires")
+	})
+
+	<-time.After(900 * time.Millisecond)
+	// Stop the timer before it fires
+	t.Stop()
+
+	// Output:
+	//
+}
